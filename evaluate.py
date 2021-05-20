95691c6bf8cdbd2e1fa4ae0f82c7e700557a1c19
redwankarimsony <redwankarimsony1455@gmail.com>
2021-05-20 09:30:31 +0600

    ____________

--- evaluate.py
+++ evaluate.py
@@ -10,6 +10,8 @@ import pandas as pd
 import seaborn as sns
 import tensorflow as tf
 from keras.models import load_model
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
 from sklearn.metrics import classification_report
 
 from dataset import load_dataset
@@ -28,51 +30,34 @@ def calc_accuracy(model, data):
     return result
 
 
-def evaluate(config_file="config.json"):
-    # Loading the configuration file
-    config = json.load(open(config_file, 'r'))
-
-    # Loading the test_datagen
-    _, _, test_generator = load_dataset()
-    print(f"[INFO] Total Number of Test instances: {len(test_generator) * config['batch_size']}")
-
-    # Loading the Saved Model
-    model = load_model(os.path.join(config['checkpoint_filepath'], 'saved_model'))
-    model.summary()
-
+def evaluate(y_true=None, y_pred=None, config_dict=None):
     # Calculating the Accuracy
-    print(calc_accuracy(model, test_generator))
+    acc_score = accuracy_score(y_true, y_pred)
+    mse = mean_squared_error(y_true, y_pred)
+    report = classification_report(y_true, y_pred)
+
+    eval_report = '_____________________CLASSIFICATION REPORT____________________________\n'
+    eval_report = eval_report + '\n' + f"Classification Accuracy: {acc_score}"
+    eval_report = eval_report + '\n' + f"Mean Squared Error     : {mse}"
+    eval_report = eval_report + '\n______________________________________________________________________\n'
+    eval_report = eval_report + report
+    print(type(report))
+    print(eval_report)
+    report_file_path = os.path.join(config_dict['checkpoint_filepath'], 'classification_report.txt')
+
+    with open(report_file_path, "w") as outfile:
+        outfile.write(eval_report)
+        print(f"[INFO] Classification report is written in the file \'{report_file_path}\'")
+        outfile.close()
 
 
-def plot_confusion_matrix(config_file="config.json"):
-    # Loading the configuration file
-    config = json.load(open(config_file, 'r'))
-
-    # Loading the test_datagen
-    _, _, test_generator = load_dataset()
-    print(f"[INFO] Total Number of Test instances: {len(test_generator) * config['batch_size']}")
-
-    # Loading the Saved Model
-    model = load_model(os.path.join(config['checkpoint_filepath'], 'saved_model'))
-    model.summary()
-
-    # Generating Predictions on test images
-    y_true, y_pred = [], []
-    no_of_batches = len(test_generator)
-    print(f"[INFO] Calculating predictions on test images")
-    for i in range(no_of_batches):
-        X, y = test_generator.next()
-        y_pred_prob = model.predict(X)
-        y_pred_batch = np.argmax(y_pred_prob, axis=1)
-        y_true = y_true + list(y)
-        y_pred = y_pred + list(y_pred_batch)
-    y_true, y_pred = np.array(y_true), np.array(y_pred)
-
+def plot_confusion_matrix(y_true=None, y_pred=None, classes=None, config_dict=None):
     # Calculating the confusion matrix
     con_mat = tf.math.confusion_matrix(labels=y_true, predictions=y_pred).numpy()
     con_mat_norm = np.around(con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], decimals=2)
     print("\n\n___________________ Confusion Matrix _____________________")
     print(con_mat_norm)
+    print("______________________________________________________________")
 
     # Finding out the class names for labeling purpose
     classes = list(test_generator.class_indices.keys())
@@ -88,7 +73,8 @@ def plot_confusion_matrix(config_file="config.json"):
     plt.xlabel('Predicted label')
     ax.set_xticklabels(textwrap.fill(x.get_text(), 20) for x in ax.get_xticklabels())
     ax.set_yticklabels(textwrap.fill(x.get_text(), 20) for x in ax.get_yticklabels())
-    filepath = os.path.join(config['checkpoint_filepath'], 'graphs', f"4.confusion-matrix{config['fig_format']}")
+    filepath = os.path.join(config_dict['checkpoint_filepath'], 'graphs',
+                            f"4.confusion-matrix{config_dict['fig_format']}")
     plt.savefig(filepath)
     print(f"\n\n[INFO] Confusion Matrix is saved in \"{filepath}\"")
 
@@ -97,29 +83,14 @@ def plot_confusion_matrix(config_file="config.json"):
     print(classification_report(y_true, y_pred))
 
 
-def find_misclassified(config_file="config.json"):
-    # Loading the configuration file
-    config = json.load(open(config_file, 'r'))
-
-    # Loading the test_datagen
-    _, _, test_generator = load_dataset()
-    print(f"[INFO] Total Number of Test instances: {len(test_generator) * config['batch_size']}")
-
-    # Loading the Saved Model
-    model = load_model(os.path.join(config['checkpoint_filepath'], 'saved_model'))
-    model.summary()
-
-    # Generating Predictions
-    print(f"[INFO] Creating predictions...")
-    pred_prob = model.predict(test_generator)
-    y_pred = np.argmax(pred_prob, axis=1).astype(int)
-    y_true = np.array(test_generator.classes).astype(int)
-    class_labels = list(test_generator.class_indices.keys())
-    file_paths = test_generator.filepaths
-    print(f"[INFO] Prediction generation complete !")
+def find_misclassified(y_true=None, y_pred=None, file_paths=None, config_dict=None):
+    if config is None:
+        print(f"\n\n[ERROR] No config dictionary found. \n Process Aborted!")
+        return
+    else:
+        classification_dir = os.path.join(config_dict['checkpoint_filepath'], 'misclassified')
 
     # Removing the old directory or creating the new one
-    classification_dir = os.path.join(config['checkpoint_filepath'], 'misclassified')
     if os.path.exists(classification_dir):
         shutil.rmtree(classification_dir)
         print(f"[INFO] Removing the old \'{classification_dir}\' directory")
@@ -140,10 +111,33 @@ def find_misclassified(config_file="config.json"):
             img = cv2.putText(img, f"Prediction: {class_labels[prediction]}", (2, 48), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                               (0, 0, 255), 1, cv2.LINE_AA)
             cv2.imwrite(os.path.join(classification_dir, class_labels[ground_truth] + new_filename + '.jpg'), img)
-            print(os.path.join(classification_dir, class_labels[ground_truth] + new_filename + '.jpg'))
+            print("Misclassified: ",
+                  os.path.join(classification_dir, class_labels[ground_truth] + new_filename + '.jpg'))
 
 
 if __name__ == "__main__":
-    evaluate()
-    plot_confusion_matrix()
-    find_misclassified()
+    # Loading the configuration file
+    config = json.load(open('config.json', 'r'))
+
+    # Loading the test_datagen
+    _, _, test_generator = load_dataset()
+    print(f"[INFO] Total Number of Test instances: {len(test_generator) * config['batch_size']}")
+
+    # Loading the Saved Model
+    model = load_model(os.path.join(config['checkpoint_filepath'], 'saved_model'))
+    model.summary()
+
+    # Generating Predictions
+    print(f"[INFO] Generating predictions...")
+    pred_prob = model.predict(test_generator)
+    y_pred = np.argmax(pred_prob, axis=1).astype(int)
+    y_true = np.array(test_generator.classes).astype(int)
+
+    # Getting the class names and file paths of the dataloader
+    class_labels = list(test_generator.class_indices.keys())
+    file_paths = test_generator.filepaths
+    print(f"[INFO] Prediction generation complete !")
+
+    evaluate(y_true=y_true, y_pred=y_pred, config_dict=config)
+    # plot_confusion_matrix(y_true=y_true, y_pred=y_pred, classes=class_labels, config_dict=config)
+    # find_misclassified(y_true=y_true, y_pred=y_pred, file_paths=file_paths, config_dict=config)
